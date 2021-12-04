1a2048752e9991d00bf91b5edcc4018bb370f0c2
reizkian <reizkianyesaya@gmail.com>
2021-12-04 07:57:06 +0700

    calibrate GMT+07 datetime util method

--- functions/util/method.js
+++ functions/util/method.js
@@ -1,23 +1,31 @@
-exports.getCurrentTime = function() {
-  var currentdate = new Date();
+exports.getCurrentTime = function(req, res) {
+  // ~ calibrated for GMT+07
+  var currentdate_calibrated_miliseconds = Date.now() + (1000*60*60*7);
+  var currentdate_calibrated = new Date(currentdate_calibrated_miliseconds);
   var datetime =
-    currentdate.getDate() +
+    currentdate_calibrated.getDate() +
     "-" +
-    (currentdate.getMonth() + 1) +
+    (currentdate_calibrated.getMonth() + 1) +
     "-" +
-    currentdate.getFullYear() +
+    currentdate_calibrated.getFullYear() +
     "@" +
-    (currentdate.getHours() + 7) +
+    currentdate_calibrated.getHours() +
     ":" +
-    currentdate.getMinutes() +
+    currentdate_calibrated.getMinutes() +
     ":" +
-    currentdate.getSeconds() + 
+    currentdate_calibrated.getSeconds() +
     ":GMT+07";
+
+  // console.log("uncalibrated miliseconds: ", Date.now());
+  // console.log("--calibrated miliseconds: ", currentdate_calibrated_miliseconds);
+  // console.log("----------------------------------------------------")
+  // console.log("uncalibrated date: ", new Date(Date.now()));
+  // console.log("--calibrated date: ", datetime);
   return datetime;
 };
 
-exports.getCompoundID = function(object, userID ){
-  const time = new Date().valueOf()
-  const compoundID = time+ "_" + object + "_" +userID;
+exports.getCompoundID = function(object, userID) {
+  const time = new Date().valueOf();
+  const compoundID = time + "_" + object + "_" + userID;
   return compoundID;
 };
