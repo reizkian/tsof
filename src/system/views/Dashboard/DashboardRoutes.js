14787b0e065c5188ee65b69adaa08bd9b46f23ef
reizkian <reizkianyesaya@gmail.com>
2021-12-12 20:49:13 +0700

    bug fixed on 404 trigger route

--- src/system/views/Dashboard/DashboardRoutes.js
+++ src/system/views/Dashboard/DashboardRoutes.js
@@ -1,13 +1,13 @@
 import { Navigate, useRoutes } from "react-router-dom";
 
 import DashboardLayout from "./layouts/dashboard";
-import LogoOnlyLayout from './layouts/LogoOnlyLayout';
-
 import DashboardApp from "./pages/DashboardApp";
-import Products from './pages/Products';
-import Blog from './pages/Blog';
-import User from './pages/User';
-import NotFound from './pages/Page404';
+import Products from "./pages/Products";
+import Blog from "./pages/Blog";
+import User from "./pages/User";
+
+import LogoOnlyLayout from "./layouts/LogoOnlyLayout";
+import NotFound from "./pages/Page404";
 
 export default function DashboardRoutes() {
   return useRoutes([
@@ -23,13 +23,15 @@ export default function DashboardRoutes() {
       ],
     },
     {
-      path: '/',
+      path: "/",
       element: <LogoOnlyLayout />,
       children: [
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> }
-      ]
+        { path: "signin" },
+        { path: "register" },
+        { path: "404", element: <NotFound /> },
+        { path: "*", element: <Navigate to="/404" /> },
+      ],
     },
-    { path: '*', element: <Navigate to="/404" replace /> }
+    { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 }
