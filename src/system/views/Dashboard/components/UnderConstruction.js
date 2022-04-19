ad0a3b5c09420029553a8dd1ffa5f311e8d2f5ae
reizkian <reizkianyesaya@gmail.com>
2022-04-19 11:53:55 +0700

    create UnderConstruction component for unfinished features

---
+++ src/system/views/Dashboard/components/UnderConstruction.js
@@ -0,0 +1,60 @@
+import { motion } from "framer-motion";
+import { Link as RouterLink } from "react-router-dom";
+// material
+import { styled } from "@mui/material/styles";
+import { Box, Button, Typography, Container } from "@mui/material";
+// components
+import { MotionContainer, varBounceIn } from "../components/animate";
+import Page from "../components/Page";
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Page)(({ theme }) => ({
+  display: "flex",
+  minHeight: "100%",
+  alignItems: "center",
+  paddingTop: theme.spacing(0),
+  paddingBottom: theme.spacing(10),
+  palette: {
+    primary: {
+      light: "#71d689",
+      main: "#54c06e;",
+      dark: "#4cb365",
+      contrastText: "#fff",
+    },
+  },
+}));
+
+export default function UnderConstruction({page}) {
+  return (
+    <>
+      <div style={{ marginTop: "0%" }}>
+        <RootStyle>
+          <Container>
+            <MotionContainer initial="initial" open>
+              <Box sx={{ maxWidth: 600, margin: "auto", textAlign: "center" }}>
+                <motion.div variants={varBounceIn}>
+                  <Typography variant="h4" paragraph>
+                   {page}
+                  </Typography>
+                </motion.div>
+                <Typography  sx={{ color: "text.secondary" }}>
+                  Mohon maaf, halaman ini masih dalam tahap pengembangan
+                </Typography>
+                <div style={{ marginTop: "-5%" }}>
+                  <motion.div variants={varBounceIn}>
+                    <Box
+                      component="img"
+                      src={require("assets/img/build.png").default}
+                      sx={{ height: 500, mx: "auto", my: { xs: 5, sm: 10 } }}
+                    />
+                  </motion.div>
+                </div>
+              </Box>
+            </MotionContainer>
+          </Container>
+        </RootStyle>
+      </div>
+    </>
+  );
+}
--- src/system/views/Dashboard/pages/muird/Internalisasi.js
+++ src/system/views/Dashboard/pages/muird/Internalisasi.js
@@ -2,14 +2,17 @@ import { Box, Container, Typography } from "@mui/material";
 
 import Page from "../../components/Page";
 import PageTitle from "../../components/PageTitle";
+import UnderConstruction from "../../components/UnderConstruction";
 
 export default function Internalisasi() {
   return (
     <Page title="The School of Fire | Sistem Informasi">
-      <Container maxWidth="xl"><Box sx={{ pb: 5 }}>
-        <PageTitle>Internalisasi</PageTitle>
-        </Box>
-        </Container>
+      <Container maxWidth="xl">
+        {/* <Box sx={{ pb: 5 }}>
+          <PageTitle>Internalisasi</PageTitle>
+        </Box> */}
+        <UnderConstruction page="Internalisasi"/>
+      </Container>
     </Page>
   );
 }
