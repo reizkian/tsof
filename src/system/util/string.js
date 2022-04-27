bcea38db396780ade1e7bc715b69c417d7b80924
reizkian <reizkianyesaya@gmail.com>
2022-04-27 18:42:04 +0700

    add toBoolean function

--- src/system/util/string.js
+++ src/system/util/string.js
@@ -1,8 +1,27 @@
-function titleCase (string){
-    const newString = string.split(' ')
-    .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
-    .join(' ');
-    return newString
+function titleCase(string) {
+  const newString = string
+    .split(" ")
+    .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+    .join(" ");
+  return newString;
 }
 
-export {titleCase}
+function toBoolean(string) {
+  switch (string.toLowerCase().trim()) {
+    case "true":
+    case "yes":
+    case "1":
+      return true;
+
+    case "false":
+    case "no":
+    case "0":
+    case null:
+      return false;
+
+    default:
+      return Boolean(string);
+  }
+}
+
+export { titleCase, toBoolean };
